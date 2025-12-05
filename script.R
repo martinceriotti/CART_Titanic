ba70687ddab6e74e1a88f4151da659e45e0aebd6
mceriotti <martinceriotti@gmail.com>
2025-12-04 21:04:05 -0300

    entrega

--- script.R
+++ script.R
@@ -1,13 +1,27 @@
 library(readr)
 library(dplyr)
+library(rpart)
+library(rpart.plot)
+library(caret)    
 
+
+# Lectura de los datos
 data  <- read_csv("datos/titanic.csv")
+
+# Analisis del dataset.
+
 names(data)
 str(data)
 
+# Quitamos PassengerId porque no lo usamos para árbol.
+
 data$PassengerId <- NULL
 
-table(data$Survived)
+table(data$Survived, data$Sex)
+table(data$Sex)
+
+# Para que se entienda mejor, cambiamos 1 y 0 por si / no.
+# Convertimos a factor las variables que nos interesen para el análisis y se puedan usar como tales.
 
 new_Survived <- factor(data$Survived)
 levels(new_Survived) <- c("Murio", "Sobrevivio")
@@ -17,96 +31,89 @@ data$Pclass <- ordered(data$Pclass, levels = c("3", "2", "1"))
 
 table(data$Cabin)
 
+# El dato de cabina es muy variado. Vamos a tomar una letra como para tener grupos de cabina.
+
+na_logical <- is.na(data$Cabin)
+data$Cabin <- ifelse(na_logical, 'X', data$Cabin)
+
 char_cabin <- as.character(data$Cabin) # Convert to character
+
 
 new_Cabin <- ifelse(
   char_cabin == "",
-  # If the value is ""
   "",
-  # Keep it
   substr(char_cabin, 1, 1)
-)    # Else transform it to a substring*
+)    
 
 new_Cabin <- factor(new_Cabin)                # Convert back to a factor
 
 table(new_Cabin)                             # Inspect the result as a table
 data$Cabin <- new_Cabin
+
 data$Survived <-  new_Survived
 
+# Tratamiento de los nulos y outliers.
 
-#Are there NA Values, Outliers or Other Strange Values?
 summary(data$Age)
 hist(data$Age, breaks = 20)
 
+# Como tenemos muchos nulos, podemos usar el valor mediano. (Median: 28 )
+
 na_logical <- is.na(data$Age)
+
 new_age <- ifelse(na_logical, 28, data$Age)
 data$Age <- new_age
 
+#Tarifa
 
 boxplot(data$Fare)
 
+# Buscamos el valor mas alto.
+
 high_roller_index <- which.max(data$Fare)
 
-high_roller_index                   # Check the index
+high_roller_index                   
 
-data[high_roller_index, ]   # Use the index to check the record
+data[high_roller_index, ]   
 
 
 # ARBOLES
 
-library(rpart)
-library(rpart.plot)
-#install.packages("caret")
-library(caret)    
+# Configuramos las opciones de arbol.
 
 options(repr.plot.width = 6, repr.plot.height = 5)
 
-gender_tree <- rpart(Survived ~ Pclass + Sex,              # Predict survival based on gender
-                     data = data)        # Use the titanic training data
+gender_tree <- rpart(Survived ~  Sex,              
+                     data = data)       
+# Dibujo del Arbol
 
-prp(gender_tree,      # Plot the decision tree
-    space=4,          # (Formatting options chosen for notebook)
-    split.cex = 1.5,
-    nn.border.col=0)
+prp(gender_tree,      
+    extra=102, type=2,  xsep="/")
 
-class_tree <- rpart(Survived ~ Sex - Pclass,    # Predict survival based on gender
-                    data = data)       # Use the titanic training data
-
-prp(class_tree,      # Plot the decision tree
-    space=4,          # (Formatting options chosen for notebook)
-    split.cex = 1.2,
-    nn.border.col=0)
+# Tenemos un total de 891 pasajeros, de los cuales 549 murieron. 468 de los pasajerons que murieron 
+# eran del género masculino y 81 eran del género femenino.
+# Del total de pasajeros, el 65% eran hombres y 35% mujeres.
 
 
-complex_tree <- rpart(Survived ~ Sex + Pclass + Age + SibSp + Fare + Embarked,
-                      cp = 0.001,                 # Set complexity parameter*
+complex_tree <- rpart(Survived ~ Sex + Pclass + Fare  + Age   ,
+                      cp = 0.012,                 # Set complexity parameter*
                       data = data)       # Use the titanic training data
 
-options(repr.plot.width = 8, repr.plot.height = 8)
+options(repr.plot.width = 4, repr.plot.height = 4)
 
 prp(complex_tree, 
-    type = 1,
-    nn.border.col=0, 
-    border.col=1, 
-    cex=0.4)
+    extra=102, type=2,  xsep="/")
 
-limited_complexity_tree <- rpart(Survived ~ Sex + Pclass + Age + SibSp +Fare+Embarked,
-                                 cp = 0.001,              # Set complexity parameter
-                                 maxdepth = 5,            # Set maximum tree depth
-                                 minbucket = 5,           # Set min number of obs in leaf nodes
-                                 method = "class",        # Return classifications instead of probs
-                                 data = data)    # Use the titanic training data
+#printcp(complex_tree)
+#plotcp(complex_tree, upper = "splits")  
 
-options(repr.plot.width = 6, repr.plot.height = 6)
+#Esto te muestra:
+#El error de cross-validation vs. tamaño del árbol.
+#El punto donde el árbol deja de mejorar.
 
-prp(limited_complexity_tree,
-    space=4,          
-    split.cex = 1.2,
-    nn.border.col=0)
+# Predicciones.
 
-
-
-train_preds <- predict(limited_complexity_tree, 
+train_preds <- predict(complex_tree, 
                        newdata = data, 
                        type = "class")               # Return class predictions
 
